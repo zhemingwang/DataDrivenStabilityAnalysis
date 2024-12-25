8945593eb5efd6d6052fdcc4d438be6edbc92a26
HanLi0806 <HanLi0806@aliyun.com>
2024-12-25 14:40:55 +0800

    'update'

--- src/RandomTrajectories.jl
+++ src/RandomTrajectories.jl
@@ -26,3 +26,24 @@ for i=1:numTraj
 end
 return y
 end
+
+function generate_trajectories_with_noise(horizon, A, C, numTraj, noise_bound_w, noise_bound_v)
+    numMode = size(A, 1)  
+    dim = size(A[1], 1)   
+    dimOut = size(C[1], 1)   
+
+    y = zeros(dimOut * horizon, numTraj)
+
+    for i = 1:numTraj
+        z0 = randn(dim) 
+        x = z0/norm(z0) 
+        b = rand((1:numMode),horizon) 
+        for j = 1:horizon
+            w = 2 * noise_bound_w * rand(dim) .- noise_bound_w
+            v = 2 * noise_bound_v * rand(dimOut) .- noise_bound_v   
+            x = A[b[j]] * x + w
+            y[(j-1)*dimOut+1:j*dimOut, i] = C[b[j]] * x + v  
+        end
+    end
+    return y
+end
